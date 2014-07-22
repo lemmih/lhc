58330dc285b031042905f67a2d27e07b564fdfa6
Lemmih <lemmih@gmail.com>
2014-07-22 17:07:24 +0800

    lhc-prim package.

---
+++ packages/lhc-prim/src/LHC/Prim.hs
@@ -0,0 +1,51 @@
+{-# LANGUAGE MagicHash, ForeignFunctionInterface #-}
+module LHC.Prim
+    ( I8
+    , I64
+    , Addr
+    ) where
+
+data I8
+data I32
+data I64
+data Addr a
+
+data CInt = CInt I32
+
+data RealWorld = RealWorld
+
+data IOUnit a = IOUnit a RealWorld
+--newtype IO a = IO { unIO :: RealWorld -> IOUnit a }
+newtype IO a = IO (RealWorld -> IOUnit a)
+
+data Unit = Unit
+
+unIO :: IO a -> RealWorld -> IOUnit a
+unIO action =
+    case action of
+        IO unit -> unit
+
+--bindIO :: IO a -> (a -> IO b) -> IO b
+--bindIO f g = IO (\s ->
+--  case unIO f s of
+--    IOUnit a s' -> unIO (g a) s')
+
+--thenIO :: IO a -> IO b -> IO b
+--thenIO a b = bindIO a (\c -> b)
+
+--return :: a -> IO a
+--return a = IO (\s -> IOUnit a s)
+
+unsafePerformIO :: IO a -> a
+unsafePerformIO action =
+    case unIO action RealWorld of
+        IOUnit val st -> val
+
+foreign import ccall unsafe "puts" puts :: Addr I8 -> IO CInt
+
+main :: IO CInt
+main = puts "Hello world!"#
+
+entryPoint :: CInt
+entryPoint = unsafePerformIO main
+
