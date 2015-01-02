2757e069b3bd2177a62440879f1c8547b06d883a
David Himmelstrup <lemmih@gmail.com>
2015-01-02 21:37:14 +0700

    Play around with the lhc-prim package.

--- packages/lhc-prim/src/LHC/Prim.hs
+++ packages/lhc-prim/src/LHC/Prim.hs
@@ -14,7 +14,11 @@ module LHC.Prim
     -- , sequence_
     , unpackString#
     , putStr
+    , getChar
+    , getLine
     , Char
+    , Bool(True,False)
+    --, not
     ) where
 
 -- XXX: These have kind # but it is not checked anywhere.
@@ -26,7 +30,9 @@ data Addr a
 
 data Int32 = Int32 I32
 
-data Char = C# I64
+data Int = I# I32
+
+data Char = C# I32
 
 data RealWorld#
 
@@ -72,13 +78,40 @@ foreign import ccall unsafe indexI8# :: Addr I8 -> I8
 foreign import ccall addrAdd# :: Addr I8 -> I64 -> Addr I8
 foreign import ccall "cast" i64toi32 :: I64 -> I32
 foreign import ccall "cast" i8toi64 :: I8 -> I64
+foreign import ccall "cast" i8toi32 :: I8 -> I32
+foreign import ccall "cast" i32to64 :: I32 -> I64
 
 unpackString# :: Addr I8 -> List Char
 unpackString# ptr =
   case i8toi64 (indexI8# ptr) of
     0#   -> Nil
-    char -> Cons (C# char) (unpackString# (addrAdd# ptr 1#))
+    char -> Cons (C# (i64toi32 char)) (unpackString# (addrAdd# ptr 1#))
 
+foreign import ccall "getchar" c_getchar :: IO Int32
+
+getChar :: IO Char
+getChar =
+    c_getchar `bindIO` \c ->
+    case c of
+        Int32 c# -> return (C# c#)
+
+getLine :: IO (List Char)
+getLine =
+  getChar `bindIO` \c ->
+  case c of
+    C# c# ->
+      case c# of
+        '\n'# -> return Nil
+        _     -> getLine `bindIO` \cs -> return (Cons c cs)
+
+--reverse :: List a -> List a
+--reverse lst = reverse_go Nil lst
+
+--reverse_go :: List a -> List a -> List a
+--reverse_go acc lst =
+--    case lst of
+--        Nil -> acc
+--        Cons x xs -> reverse_go (Cons x acc) xs
 
 putStr :: List Char -> IO Unit
 putStr lst =
@@ -87,4 +120,50 @@ putStr lst =
     Cons head tail ->
       case head of
         C# char ->
-          putchar (i64toi32 char) `thenIO` putStr tail
+          putchar char `thenIO` putStr tail
+
+data Bool = False | True
+
+not :: Bool -> Bool
+not x = case x of
+   False -> True
+   True  -> False
+
+otherwise :: Bool
+otherwise = True
+
+--intAdd :: Int -> Int -> Int
+--intAdd = intAdd
+
+--intSub :: Int -> Int -> Int
+--intSub = intSub
+
+--intMul :: Int -> Int -> Int
+--intMul = intMul
+
+--seq :: a -> b -> b
+--seq a b = b
+
+--bit :: Int -> Int
+--bit = bit
+-- bit n = 1<<n
+
+mapM_ :: (a -> IO Unit) -> List a -> IO Unit
+mapM_ fn lst =
+  case lst of
+    Nil -> return Unit
+    Cons x xs -> fn x `thenIO` mapM_ fn xs
+
+-- <=
+--le :: Int -> Int -> Bool
+--le = le
+
+--max :: Int -> Int -> Int
+max = max
+
+append :: List a -> List a -> List a
+append a b =
+  case a of
+    Nil       -> b
+    Cons x xs -> Cons x (append xs b)
+
