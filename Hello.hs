322459f5793e8ed0481c39c6e68bf1a2d6120720
David Himmelstrup <lemmih@gmail.com>
2018-04-01 20:27:09 +0200

    Use do-notation in the HelloWorld example

--- Hello.hs
+++ Hello.hs
@@ -4,19 +4,12 @@ module Main (main) where
 import LHC.Prim
 
 -- main :: IO ()
--- main = do
---   putStrLn (unpackString# "What is your name?"#)
---   name <- getLine
---   putStr (unpackString# "Hi "#)
---   putStr name
---   putStrLn (unpackString# "."#)
-
-main =
-  putStrLn (unpackString# "What is your name?"#) `thenIO`
-  (getLine `bindIO` \name ->
-  (putStr (unpackString# "Hi "#) `thenIO`
-  (putStr name `thenIO`
-  (putStrLn (unpackString# "."#)))))
+main = do
+  putStrLn (unpackString# "What is your name?"#)
+  name <- getLine
+  putStr (unpackString# "Hi "#)
+  putStr name
+  putStrLn (unpackString# "."#)
 
 entrypoint :: ()
 entrypoint = unsafePerformIO main
