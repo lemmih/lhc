e2eae23c0741664e12168dbc772d40ee12dcb1f7
David Himmelstrup <lemmih@gmail.com>
2018-04-04 23:28:17 +0200

    Add bedrock documentation.

--- bedrock/src/Data/Bedrock.hs
+++ bedrock/src/Data/Bedrock.hs
@@ -1,12 +1,84 @@
-{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DeriveGeneric      #-}
+{-|
+Bedrock is a strict, first-order language designed for graph reduction.
+
+The language supports high-level features such as graph reduction (via 'eval'
+and 'apply' primitives), an implicit stack (via function application and function
+return), exceptions (via the 'catch', 'raise' and 'invoke' primitives), automatic
+memory management (via the 'store' and 'alloc' primitives). Each of these
+primitives can be removed by code transformations that maintain the semantic
+properties of the code.
+
+The code transformations work roughly like this:
+
+ * Lowering 'eval' and 'apply' using heap-points-to (HPT) analysis.
+   HPT analysis yields a conservative set of possible nodes for each variable in
+   the bedrock code. Appropriate 'eval' functions and 'apply' functions can then
+   be generated for each use of the 'eval' and 'apply' primitives. An 'eval'
+   function might look like this:
+
+   > eval ptr = do
+   >   node <- fetch ptr
+   >   ret <- case node of
+   >     FunctionNode arg1 arg2 -> function arg1 arg2
+   >   update ptr (Indirection ret)
+   >   return ret
+
+ * Lowering 'eval' and 'apply' can also be done without an HPT analysis (this is
+   approach currently implemented). This is necessarily much more conservative.
+   The bedrock code is scanned once to find all possible suspected functions and
+   all possible function applications by type. A single @eval@ function is then
+   generated and 'apply' functions are generated for each argument type / return
+   type pair. For example:
+
+@
+   apply_i8 ptr i8 = do
+     node <- fetch ptr
+     case node of
+       Function arg1 _ -> function arg1 i8
+       PartialFunction _ _ -> return (PartialFunction i8 _)
+@
+
+  * The stack primitives are removed by a CPS transformation. Activation frames
+    are made explicit and can be traversed by the exception handling code.
+    For example:
+
+@
+    function = do
+      a <- callFnA
+      b <- callFnB
+      store (Tuple a b)
+@
+
+    Would be transformed into:
+
+@
+    function = do
+      push function_part2
+      tailcall callFnA
+    function_part2 a =
+      push (function_part3 a)
+      tailcall callFnB
+    function_part3 a b =
+      store (Tuple a b)
+@
+
+  * Memory management primitives are removed by plugging in memory management
+    library. Right now the only library available just allocations a large slab
+    of memory when executables boot up and never performs any garbage collection.
+
+Once all the high-level primitives have been transformed out of the bedrock code,
+LLVM code can be trivially generated.
+-}
 module Data.Bedrock where
 
-import Data.Data
-import GHC.Generics
-import Test.QuickCheck (Arbitrary(..), listOf, elements, listOf1, oneof, suchThat)
-import Control.Applicative ((<$>), (<*>), pure )
-import qualified LLVM.AST.Type                   as LLVM
+import           Control.Applicative (pure, (<$>), (<*>))
+import           Data.Data
+import           GHC.Generics
+import qualified LLVM.AST.Type       as LLVM
+import           Test.QuickCheck     (Arbitrary (..), elements, listOf, listOf1,
+                                      oneof, suchThat)
 
 data Name = Name
   { nameModule     :: [String]
@@ -48,16 +120,16 @@ data CType
   deriving (Show, Read, Eq, Ord, Data, Generic)
 
 data Foreign = Foreign
-  { foreignName :: String
-  , foreignReturn :: CType
+  { foreignName      :: String
+  , foreignReturn    :: CType
   , foreignArguments :: [CType]
   } deriving (Show, Read, Data, Generic)
 
 data Module = Module
-  { modForeigns :: [Foreign]
-  , nodes      :: [NodeDefinition]
-  , entryPoint :: Name
-  , functions  :: [Function]
+  { modForeigns  :: [Foreign]
+  , nodes        :: [NodeDefinition]
+  , entryPoint   :: Name
+  , functions    :: [Function]
   , modNamespace :: AvailableNamespace
   -- CAFs?
   } deriving (Show, Read, Data, Generic)
@@ -99,7 +171,7 @@ data Literal
   deriving (Show, Read, Eq, Data, Generic)
 
 data MemAttributes = MemAttributes
-  { memConstant :: Bool
+  { memConstant   :: Bool
   , memAliasGroup :: Maybe Int
   } deriving (Show, Read, Eq, Data, Generic)
 
