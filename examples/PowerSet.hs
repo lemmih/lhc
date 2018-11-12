8aa1f601b2462f32e9a1df542e1d46191d0bc919
lemmih <lemmih@gmail.com>
2018-11-12 18:45:10 +0100

    Add yet another example to stress the GC.

---
+++ examples/PowerSet.hs
@@ -0,0 +1,20 @@
+module Main where
+
+import LHC.Prim
+import LHC.Prelude
+
+run :: Int -> [[Int]]
+run n = power n n []
+
+power :: Int -> Int -> [Int] -> [[Int]]
+power 0 0 acc = acc : []
+power n 0 acc = acc : power (n-1) (n `sdiv` 2) acc
+power n i acc = power n (i-1) (i:acc)
+
+main :: IO ()
+main = do
+  putStrLn "Allocating a bunch of data"
+  putStrLn (showInt (last (last (reverse (run 8000)))))
+
+entrypoint :: ()
+entrypoint = unsafePerformIO main
