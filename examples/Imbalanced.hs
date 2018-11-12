23e5b054ff60bd69c42fa28809f5da2d00ae31f4
lemmih <lemmih@gmail.com>
2018-11-12 18:33:03 +0100

    Add another example problem.

---
+++ examples/Imbalanced.hs
@@ -0,0 +1,29 @@
+module Main where
+
+import LHC.Prim
+import LHC.Prelude
+
+data Tree = Leaf | Branch Tree Tree
+
+make :: Int -> Tree
+make 0 = Leaf
+make 1 = Branch Leaf Leaf
+make n = Branch (make (n-2)) (make (n-1))
+
+check :: Tree -> Int
+check Leaf = 0
+check (Branch l r) = 1 + check r + check l
+
+main :: IO ()
+main = dup (make 33)
+
+dup a = act a a
+
+act :: Tree -> Tree -> IO ()
+act a b = do
+  putStrLn (showInt (check a))
+  putStrLn (showInt (check b))
+
+
+entrypoint :: ()
+entrypoint = unsafePerformIO main
